4caf2af85c13c36b931d74837a35d60da0e6d01a
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2024-02-05 20:26:17 +0100

    Liczba losowa

---
+++ SmartCodes2019/RandomDigit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Math;
+
+namespace RandomDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            double losuj_liczbę, zgadnij_liczbę;
+
+            Console.WriteLine("Program losuje liczbę od 0 do 9. Zgadnij ją.");
+
+            losuj_liczbę = Round(10 * (r.NextDouble()));
+            zgadnij_liczbę = double.Parse(Console.ReadLine());
+
+            if (zgadnij_liczbę == losuj_liczbę)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Gratulacje! Zgadłeś liczbę!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Bardzo mi przykro, ale wylosowana liczba to {0}.", losuj_liczbę);
+                Console.ResetColor();
+            }
+        }
+    }
+}
